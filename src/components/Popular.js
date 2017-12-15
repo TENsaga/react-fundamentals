ebc22ce7de892860ada30c24c4b15aecbf04c645
TENsaga <munin@gmx.com>
2017-12-15 15:57:03 -0800

    Splitting up Popular Component, updated css

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const SelectLanguage = ({ selectedLanguage, onSelect }) => {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'].map(
+    lang => (
+      <li
+        key={lang}
+        style={lang === selectedLanguage ? { color: '#d0021b' } : null}
+        onClick={onSelect.bind(null, lang)}
+      >
+        {lang}
+      </li>
+    )
+  );
+  return <ul className="languages">{languages}</ul>;
+};
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends Component {
   constructor(props) {
@@ -13,26 +34,14 @@ class Popular extends Component {
   };
 
   render() {
-    const languages = [
-      'All',
-      'JavaScript',
-      'Ruby',
-      'Java',
-      'CSS',
-      'Python'
-    ].map(lang => (
-      <li
-        key={lang}
-        style={
-          lang === this.state.selectedLanguage ? { color: '#d0021b' } : null
-        }
-        onClick={this.updateLanguage.bind(null, lang)}
-      >
-        {lang}
-      </li>
-    ));
-
-    return <ul className="languages">{languages}</ul>;
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
+    );
   }
 }
 
