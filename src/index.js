afcf733ab2ff868a4310e2424959013c7933a24a
TENsaga <munin@gmx.com>
2017-12-15 09:43:01 -0800

    Changed dir structure, modified Webpack configuration, added Sass, successful build

--- src/index.js
+++ src/index.js
@@ -18,5 +18,5 @@ class App extends React.Component {
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,45 @@
-module.export = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+module.exports = {
   entry: [
-    './app/index.js'
+    './src/index.js'
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,  // which files to parse
-        exclude: /node_modules/,  // which files to skip
-        use: 'babel-loader', preset: [ 'env', 'react' ]  // use tells webpack which loader to use
-      }
-    ]
-  },
   output: { // output folder will be ourApp/dist/index_bundle.js
     path: path.resolve(__dirname, 'dist'),  // dirname references the current directory the script is run in
     filename: 'index_bundle.js'
   },
-  plugins: {
-    newHtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  // which files to parse
+        exclude: /node_modules/,  // which files to skip
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    extractPlugin
+  ]
 }
